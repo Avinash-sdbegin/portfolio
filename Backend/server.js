31a2ccfc1c9f14d7b3a624c4d22ee17d53f80132
Avinash Kumar <avinashkumarmgr24@gmail.com>
2026-03-24 16:25:23 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -27,17 +27,12 @@ if (!EMAIL_USER || !EMAIL_PASS) {
 }
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, 
+  service: "gmail",
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASS,
   },
-  connectionTimeout: 10000, 
-  tls: {
-    rejectUnauthorized: false 
-  }
+  family: 4,
 });
 app.use(
   cors({
